ff1274a85540d596c81c97fb8ef1dd50b7d0b82d
Cosmin <floreacosmin06@gmail.com>
2026-05-09 23:14:04 +0000

    Metodo Codespaces

---
+++ src/main/java/it/uniroma1/mdp/uno/view/Launcher.java
@@ -0,0 +1,8 @@
+package it.uniroma1.mdp.uno.view;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Metodo da lanciare in Codespaces
+        MainApp.main(args);
+    }
+}
