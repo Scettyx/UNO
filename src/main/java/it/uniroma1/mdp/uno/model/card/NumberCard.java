d4a1e7e70324fa57ea2662e0e977fc30eca58cc0
lexyp2906 <giorginimassimo29@gmail.com>
2026-05-11 10:00:51 +0200

    updated NumberCard

--- src/main/java/it/uniroma1/mdp/uno/model/card/NumberCard.java
+++ src/main/java/it/uniroma1/mdp/uno/model/card/NumberCard.java
@@ -7,15 +7,7 @@ package it.uniroma1.mdp.uno.model.card;
 public class NumberCard extends Card{
 
 	protected NumberCard(CardType type, CardColor color, CardNumber number, int value) {
-		super(type.NUMBER, color, number, value );
-
+		super(type.NUMBER, color, number, number.getNumber() );
 	}
-	
-						
-	
-    	
-    
-    
-	
 
 }
